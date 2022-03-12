d6749bed90efd31bcdffaef4bb42e37974e71f75
Federico <codepalace@fastmail.se>
2022-03-12 10:54:37 +0100

    First

--- GridViewDemo/ContentView.swift
+++ GridViewDemo/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let data: [Int] = Array(1...20)
+    private let colors: [Color] = [.red, .green, .blue, .yellow]
+    
+    // Flexible, custom amount of columns that fill the remaining space
+    private let numberColumns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    // Adaptive, make sure it's the size of your smallest element.
+    private let adaptiveColumns = [
+        GridItem(.adaptive(minimum: 170))
+    ]
+    
+    // Fixed, creates columns with fixed dimensions
+    private let fixedColumns = [
+        GridItem(.fixed(200)),
+        GridItem(.fixed(200))
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: adaptiveColumns, spacing: 20) {
+                    ForEach(data, id: \.self) { number in
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 170, height: 170)
+                                .foregroundColor(colors[number%4])
+                                .cornerRadius(30)
+                            Text("\(number)")
+                                .foregroundColor(.white)
+                                .font(.system(size: 70, design: .rounded))
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Sample Grids")
             .padding()
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portraitUpsideDown)
     }
 }
