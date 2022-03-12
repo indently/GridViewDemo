8e6bea8c0880d1b983a268bbe5ba6369ccce0c27
Federico <codepalace@fastmail.se>
2022-03-12 10:16:07 +0100

    Initial Commit

---
+++ GridViewDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GridViewDemo
+//
+//  Created by Federico on 12/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GridViewDemo/GridViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridViewDemoApp.swift
+//  GridViewDemo
+//
+//  Created by Federico on 12/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct GridViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
